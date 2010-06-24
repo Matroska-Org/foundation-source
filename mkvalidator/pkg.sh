cdbdafacd42ef884ca4f86d80801a9f29c5a2477
Steve Lhomme <slhomme@matroska.org>
2010-06-24 13:29:49 +0000

    mkclean/mkvalidator: add a script to make it easy to create the source package
    git-svn-id: https://matroska.svn.sourceforge.net/svnroot/matroska/trunk/foundation_src@335 a6f86f6d-0131-4f8e-9e7b-e335508773d5

---
+++ mkvalidator/pkg.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+%%BEGIN
+SCRIPT
+%%END
+
+rm -rf %(PROJECT_NAME)-%(PROJECT_VERSION)
+branch src.br
+rm -rf %(PROJECT_NAME)-%(PROJECT_VERSION).tar.bz2
+tar cvfj %(PROJECT_NAME)-%(PROJECT_VERSION).tar.bz2 %(PROJECT_NAME)-%(PROJECT_VERSION)
