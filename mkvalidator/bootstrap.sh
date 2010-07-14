15cb8003828f8025d43a2b19f4a04629b320f98a
Steve Lhomme <slhomme@matroska.org>
2010-07-14 10:33:21 +0000

    detect the OS/CPU in the bootstraps (needs to be used)
    git-svn-id: https://matroska.svn.sourceforge.net/svnroot/matroska/trunk/foundation_src@409 a6f86f6d-0131-4f8e-9e7b-e335508773d5

--- mkvalidator/bootstrap.sh
+++ mkvalidator/bootstrap.sh
@@ -5,6 +5,12 @@ SCRIPT
 
 make -s -f coremake.mak coremake
 
+cpu_name=`uname -m | sed y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/`
+os_name=`uname -s | sed y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/`
+
+echo "CPU" $cpu_name
+echo "OS" $os_name
+
 echo "Running ./coremake gcc_linux"
 ./coremake gcc_linux
 
