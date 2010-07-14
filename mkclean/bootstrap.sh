359c5073e302f244a2c8394481a7e8f71205b219
Steve Lhomme <slhomme@matroska.org>
2010-07-14 11:15:22 +0000

    bootstrap.sh should now call the correct coremake config depending on the host platform
    git-svn-id: https://matroska.svn.sourceforge.net/svnroot/matroska/trunk/foundation_src@410 a6f86f6d-0131-4f8e-9e7b-e335508773d5

--- mkclean/bootstrap.sh
+++ mkclean/bootstrap.sh
@@ -7,11 +7,63 @@ make -s -f coremake.mak coremake
 
 cpu_name=`uname -m | sed y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/`
 os_name=`uname -s | sed y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/`
+coremake_param="gcc_linux"
 
-echo "CPU" $cpu_name
-echo "OS" $os_name
+#echo "CPU" $cpu_name
+#echo "OS" $os_name
 
-echo "Running ./coremake gcc_linux"
-./coremake gcc_linux
+case $os_name in
+cygwin*)
+  coremake_param="gcc_linux"
+  ;;
+mingw* | pw32*)
+  coremake_param="gcc_win32"
+  ;;
+darwin* | rhapsody*)
+    case $cpu_name in
+        i*86)
+            coremake_param="gcc_osx_x86"
+            ;;
+        powerpc*)
+            coremake_param="gcc_osx_ppc"
+            ;;
+        x86_64)
+            coremake_param="gcc_osx_x64"
+            ;;
+    esac
+  ;;
+freebsd* | kfreebsd*-gnu | dragonfly*)
+  coremake_param="gcc_linux"
+  ;;
+linux*)
+    case $cpu_name in
+        i*86)
+            coremake_param="gcc_linux"
+            ;;
+        x86_64)
+            coremake_param="gcc_linux_x64"
+            ;;
+        powerpc*)
+            coremake_param="gcc_linux_ppc"
+            ;;
+        arm*)
+            coremake_param="gcc_linux_arm"
+            ;;
+        mips*)
+            coremake_param="gcc_linux_mips"
+            ;;
+        sparc64)
+            coremake_param="gcc_linux_sparc64"
+            ;;
+        sparc*)
+            coremake_param="gcc_linux_sparc32"
+            ;;
+    esac
+  ;;
+esac
+
+
+echo "Running ./coremake" $coremake_param
+./coremake $coremake_param
 
 echo "Now you can run make -C %(PROJECT_NAME)"
