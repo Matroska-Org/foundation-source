f4f4b17b6466f882486ab66168a67ea9856ef4db
Steve Lhomme <slhomme@matroska.org>
2010-08-02 19:08:31 +0000

    mkclean: add a message when mkclean is run in mkWDclean mode
    git-svn-id: https://matroska.svn.sourceforge.net/svnroot/matroska/trunk/foundation_src@469 a6f86f6d-0131-4f8e-9e7b-e335508773d5

--- mkclean/mkclean.c
+++ mkclean/mkclean.c
@@ -1174,6 +1174,8 @@ int main(int argc, const char *argv[])
     Node_FromStr(&p,Path,TSIZEOF(Path),argv[0]);
     SplitPath(Path,NULL,0,String,TSIZEOF(String),NULL,0);
     UnOptimize = tcsisame_ascii(String,T("mkWDclean"));
+    if (UnOptimize)
+        TextPrintf(StdErr,T("Running special mkWDclean mode, please fix your player instead of valid Matroska files\r\n"),Path);
 
 	for (i=1;i<argc;++i)
 	{
