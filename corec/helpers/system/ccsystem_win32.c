811a68ad18d13787b6acae889573433b7ed22171
Steve Lhomme <slhomme@matroska.org>
2010-11-01 12:47:21 +0000

    Core-C: add a generic function to run command lines and get the return value
    git-svn-id: https://matroska.svn.sourceforge.net/svnroot/matroska/trunk/foundation_src@584 a6f86f6d-0131-4f8e-9e7b-e335508773d5

---
+++ corec/helpers/system/ccsystem_win32.c
@@ -0,0 +1,55 @@
+/*****************************************************************************
+ * 
+ * Copyright (c) 2010, Steve Lhomme
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of CoreCodec, Inc. nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY CoreCodec, Inc. ``AS IS'' AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL CoreCodec, Inc. BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#include "system/ccsystem.h"
+#include "parser/parser.h"
+
+#include <windows.h>
+
+int RunCommand(anynode *p, const tchar_t *Cmd, const tchar_t *CmdParams, bool_t Silent)
+{
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+    DWORD returnCode;
+    tchar_t Command[4096];
+    int Flags = Silent ? CREATE_NO_WINDOW : 0;
+
+    stprintf_s(Command,TSIZEOF(Command),T("%s %s"),Cmd, CmdParams);
+
+    ZeroMemory( &si, sizeof(si) );
+    si.cb = sizeof(si);
+    ZeroMemory( &pi, sizeof(pi) );
+
+    if (!CreateProcess(NULL,Command,NULL,NULL,FALSE,Flags,NULL,NULL,&si,&pi))
+        return -1;
+
+    while (GetExitCodeProcess(pi.hProcess, &returnCode) && returnCode==STILL_ACTIVE)
+        Sleep(100);
+    return (int)returnCode;
+}
