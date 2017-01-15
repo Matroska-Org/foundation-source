3fbb2d0e3226cbd30d5ad442d079340afeb29f54
Steve Lhomme <slhomme@matroska.org>
2017-01-15 17:43:10 +0100

    spectool: warn when the spec file is missing

--- spectool/data2lib.c
+++ spectool/data2lib.c
@@ -35,6 +35,8 @@
 #include "data2lib2_stdafx.h"
 #include "spec_element.h"
 
+#include <stdio.h>
+
 typedef struct table_extras
 {
     bool_t StartedGlobal;
@@ -506,7 +508,12 @@ int main(void)
     ParserContext_Init(&p,NULL,NULL,NULL);
     StdAfx_Init((nodemodule*)&p);
 
-    Input = StreamOpen(&p,T("specdata.xml"),SFLAG_RDONLY/*|SFLAG_BUFFERED*/);
+    Input = StreamOpen(&p,T("ebml_matroska.xml"),SFLAG_RDONLY/*|SFLAG_BUFFERED*/);
+    if (Input == NULL)
+    {
+        fprintf(stderr, "missing source file ebml_matroska.xml\n");
+        return -1;
+    }
     OutputC = StreamOpen(&p,T("KaxSemantic.cpp"),SFLAG_WRONLY|SFLAG_CREATE);
     OutputH = StreamOpen(&p,T("KaxSemantic.h"),SFLAG_WRONLY|SFLAG_CREATE);
 
