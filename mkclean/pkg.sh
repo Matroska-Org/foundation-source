65ddcd206f9913e21405a6f2215a9097ca022c9b
Steve Lhomme <slhomme@matroska.org>
2017-08-22 15:06:09 +0200

    fix the branch file to use when compiling scripts

--- mkclean/pkg.sh
+++ mkclean/pkg.sh
@@ -4,6 +4,6 @@ SCRIPT
 %%END
 
 rm -rf %(PROJECT_NAME)-%(PROJECT_VERSION)
-branch src.br
+branch src.br.br
 rm -rf %(PROJECT_NAME)-%(PROJECT_VERSION).tar.bz2
 tar cvfj %(PROJECT_NAME)-%(PROJECT_VERSION).tar.bz2 %(PROJECT_NAME)-%(PROJECT_VERSION)
