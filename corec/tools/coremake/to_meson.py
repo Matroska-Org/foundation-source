afcfbbeafe12cdedef993e3cc3a5a30ced074a6b
Steve Lhomme <slhomme@matroska.org>
2018-11-18 16:35:29 +0100

    SQUASH coremake: add a tool to translate Coremake projects to Meson projects

--- corec/tools/coremake/to_meson.py
+++ corec/tools/coremake/to_meson.py
@@ -15,8 +15,22 @@ if not root.is_file():
     print("%s/root.proj is not a file" % sys.argv[1])
     exit(0)
 
+def parse_main_line(line, projfile):
+    line.strip()
+    if line.startswith("CONFIG_FILE"):
+        print("config line (SKIP)")
+    elif line.startswith("PLATFORM_FILES"):
+        print("platform files (IGNORED)")
+    # elif line.empty():
+    #     extraline = projfile.readline()
+    #     print("extra line of %s is %s" % (line, extraline))
+
 def main(root_proj):
     print(root_proj)
+    inputfile = open(root_proj)
+    for line in inputfile:
+        parse_main_line(line, inputfile)
+    inputfile.close()
 
 if __name__ == '__main__':
     sys.exit(main(root))
