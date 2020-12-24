31ba725f971799ecc6fc66fa10369c76b590abfe
Steve Lhomme <slhomme@matroska.org>
2020-12-24 09:04:51 +0100

    remove coremake based projects

--- mkvalidator/pkg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-%%BEGIN
-SCRIPT
-%%END
-
-rm -rf %(PROJECT_NAME)-%(PROJECT_VERSION)
-branch src.br.br
-rm -rf %(PROJECT_NAME)-%(PROJECT_VERSION).tar.bz2
-tar cvfj %(PROJECT_NAME)-%(PROJECT_VERSION).tar.bz2 %(PROJECT_NAME)-%(PROJECT_VERSION)
