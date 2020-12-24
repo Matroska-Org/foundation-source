3b9c1e81641d236225d72d3af528709fb7267dab
Steve Lhomme <slhomme@matroska.org>
2020-12-24 08:30:46 +0100

    corec: tools: remove coremake
    Now we can use CMake

--- corec/tools/coremake/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-make -f build.mak
-sudo mv -f coremake /usr/local/bin/coremake
-sudo mv -f corerc /usr/local/bin/corerc
-
-#sudo gcc -O2 coremake.c -o /usr/local/bin/coremake
-#sudo gcc -O2 corerc.c -o /usr/local/bin/corerc
