Matroska-Org/foundation-source

commit edeaa812964124138e989b6ca4c4aa4335683dcf

libebml2: compare element sizes with filepos_t
It's the bigger type compared to size_t.

libebml2/ebmlbinary.c
@@ -43,7 +43,7 @@ static err_t ReadData(ebml_binary *Element, stream *Input, const ebml_parser_con
         goto failed;
     }
 
-    if (Element->Base.DataSize > SIZE_MAX || !ArrayResize(&Element->Data,(size_t)Element->Base.DataSize,0))
+    if (Element->Base.DataSize > (filepos_t)SIZE_MAX || !ArrayResize(&Element->Data,(size_t)Element->Base.DataSize,0))
     {
         Result = ERR_OUT_OF_MEMORY;
         goto failed;

libebml2/ebmlmaster.c
@@ -353,7 +353,7 @@ static err_t ReadData(ebml_master *Element, stream *Input, const ebml_parser_con
                                 // read the rest of the element in memory to avoid reading it a second time later
                                 ArrayInit(&CrcBuffer);
                                 filepos_t element_size = EBML_ElementPositionEnd((ebml_element*)Element) - EBML_ElementPositionEnd(SubElement);
-                                if (element_size < SIZE_MAX && ArrayResize(&CrcBuffer, (size_t)element_size, 0))
+                                if (element_size < (filepos_t)SIZE_MAX && ArrayResize(&CrcBuffer, (size_t)element_size, 0))
                                 {
                                     CRCData = ARRAYBEGIN(CrcBuffer,uint8_t);
                                     CRCDataSize = ARRAYCOUNT(CrcBuffer,uint8_t);
@@ -497,7 +497,7 @@ static err_t RenderData(ebml_master *Element, stream *Output, bool_t bForceWitho
         array TmpBuf;
         bool_t IsMemory = Node_IsPartOf(Output,MEMSTREAM_CLASS);
         ArrayInit(&TmpBuf);
-        if (!IsMemory && ((Element->Base.DataSize - CRC_EBML_SIZE) > SIZE_MAX || !ArrayResize(&TmpBuf, (size_t)Element->Base.DataSize - CRC_EBML_SIZE, 0)))
+        if (!IsMemory && ((Element->Base.DataSize - CRC_EBML_SIZE) > (filepos_t)SIZE_MAX || !ArrayResize(&TmpBuf, (size_t)Element->Base.DataSize - CRC_EBML_SIZE, 0)))
             Err = ERR_OUT_OF_MEMORY;
         else
         {
@@ -517,7 +517,7 @@ static err_t RenderData(ebml_master *Element, stream *Output, bool_t bForceWitho
                         Node_Set(VOutput, MEMSTREAM_DATA, ARRAYBEGIN(TmpBuf,uint8_t), ARRAYCOUNT(TmpBuf,uint8_t));
                         Node_SET(VOutput, MEMSTREAM_OFFSET, &Offset);
                         Err = InternalRender(Element, VOutput, bForceWithoutMandatory, bWithDefault, ForProfile, Rendered);
-                        assert(Err!=ERR_NONE || *Rendered == ARRAYCOUNT(TmpBuf,uint8_t));
+                        assert(Err!=ERR_NONE || *Rendered == (filepos_t)ARRAYCOUNT(TmpBuf,uint8_t));
                         if (Err==ERR_NONE)
                         {
                             filepos_t CrcSize;
@@ -528,7 +528,7 @@ static err_t RenderData(ebml_master *Element, stream *Output, bool_t bForceWitho
                             {
                                 size_t Written;
                                 Err = Stream_Write(Output, ARRAYBEGIN(TmpBuf,uint8_t), ARRAYCOUNT(TmpBuf,uint8_t), &Written);
-                                assert(Err!=ERR_NONE || Written == *Rendered);
+                                assert(Err!=ERR_NONE || (filepos_t)Written == *Rendered);
                                 *Rendered = Written + CrcSize;
                             }
                         }

libebml2/ebmlstring.c
@@ -80,7 +80,7 @@ static err_t ReadData(ebml_string *Element, stream *Input, const ebml_parser_con
         Result = ERR_READ;
         goto failed;
     }
-    if (Element->Base.DataSize > SIZE_MAX - 1)
+    if (Element->Base.DataSize > (filepos_t)SIZE_MAX - 1)
     {
         Result = ERR_OUT_OF_MEMORY;
         goto failed;

commit 9f7f0c434a12e8d35bacdd7435f0055da729c101

libebml2: fix size_t vs filepos_t comparisons
Regression from edeaa812964124138e989b6ca4c4aa4335683dcf.

libebml2/ebmlbinary.c
@@ -43,7 +43,12 @@ static err_t ReadData(ebml_binary *Element, stream *Input, const ebml_parser_con
         goto failed;
     }
 
-    if (Element->Base.DataSize > (filepos_t)SIZE_MAX || !ArrayResize(&Element->Data,(size_t)Element->Base.DataSize,0))
+#if MAX_FILEPOS >= SIZE_MAX
+    if ((filepos_t)Element->Base.DataSize > (filepos_t)SIZE_MAX
+#else
+    if ((size_t)Element->Base.DataSize > (size_t)SIZE_MAX
+#endif
+        || !ArrayResize(&Element->Data,(size_t)Element->Base.DataSize,0))
     {
         Result = ERR_OUT_OF_MEMORY;
         goto failed;

libebml2/ebmlmaster.c
@@ -355,7 +355,12 @@ static err_t ReadData(ebml_master *Element, stream *Input, const ebml_parser_con
                                 // read the rest of the element in memory to avoid reading it a second time later
                                 ArrayInit(&CrcBuffer);
                                 filepos_t element_size = EBML_ElementPositionEnd((ebml_element*)Element) - EBML_ElementPositionEnd(SubElement);
-                                if (element_size < (filepos_t)SIZE_MAX && ArrayResize(&CrcBuffer, (size_t)element_size, 0))
+#if MAX_FILEPOS >= SIZE_MAX
+                                if ((filepos_t)element_size < (filepos_t)SIZE_MAX
+#else
+                                if ((size_t)element_size < (size_t)SIZE_MAX
+#endif
+                                    && ArrayResize(&CrcBuffer, (size_t)element_size, 0))
                                 {
                                     CRCData = ARRAYBEGIN(CrcBuffer,uint8_t);
                                     CRCDataSize = ARRAYCOUNT(CrcBuffer,uint8_t);
@@ -499,7 +504,13 @@ static err_t RenderData(ebml_master *Element, stream *Output, bool_t bForceWitho
         array TmpBuf;
         bool_t IsMemory = Node_IsPartOf(Output,MEMSTREAM_CLASS);
         ArrayInit(&TmpBuf);
-        if (!IsMemory && ((Element->Base.DataSize - CRC_EBML_SIZE) > (filepos_t)SIZE_MAX || !ArrayResize(&TmpBuf, (size_t)Element->Base.DataSize - CRC_EBML_SIZE, 0)))
+        if (!IsMemory &&
+#if MAX_FILEPOS >= SIZE_MAX
+            ((filepos_t)(Element->Base.DataSize - CRC_EBML_SIZE) > (filepos_t)SIZE_MAX
+#else
+            ((size_t)(Element->Base.DataSize - CRC_EBML_SIZE) > (size_t)SIZE_MAX
+#endif
+             || !ArrayResize(&TmpBuf, (size_t)Element->Base.DataSize - CRC_EBML_SIZE, 0)))
             Err = ERR_OUT_OF_MEMORY;
         else
         {

libebml2/ebmlstring.c
@@ -80,7 +80,11 @@ static err_t ReadData(ebml_string *Element, stream *Input, const ebml_parser_con
         Result = ERR_READ;
         goto failed;
     }
-    if (Element->Base.DataSize > (filepos_t)SIZE_MAX - 1)
+#if MAX_FILEPOS >= SIZE_MAX
+    if ((filepos_t)Element->Base.DataSize > (filepos_t)(SIZE_MAX - 1))
+#else
+    if ((size_t)Element->Base.DataSize > (size_t)(SIZE_MAX - 1))
+#endif
     {
         Result = ERR_OUT_OF_MEMORY;
         goto failed;