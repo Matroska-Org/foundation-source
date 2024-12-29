c4ba16459c4b141494ca0b762f96fc9a93ec2f9d
Steve Lhomme <slhomme@matroska.org>
2024-12-29 09:20:33 +0100

    corec: add DebugMessage outside of Windows
    log to stderr.

--- mkclean/mkclean.c
+++ mkclean/mkclean.c
@@ -100,6 +100,20 @@ void DebugMessage(const tchar_t* Msg,...)
 }
 #endif
 }
+#else
+#include <stdio.h>
+void DebugMessage(const tchar_t* Msg,...)
+{
+    va_list Args;
+    tchar_t Buffer[1024];
+
+    va_start(Args,Msg);
+    vstprintf_s(Buffer,TSIZEOF(Buffer), Msg, Args);
+    va_end(Args);
+    tcscat_s(Buffer,TSIZEOF(Buffer),T("\r\n"));
+
+    fprintf(stderr, "%s", Buffer);
+}
 #endif
 
 #define EXTRA_SEEK_SPACE       22
