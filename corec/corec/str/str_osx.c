220a26be37dd621ac39d653378ca00b476b319cf
Steve Lhomme <slhomme@matroska.org>
2024-12-30 08:47:49 +0100

    replace BSD-3-Clause license text in C/H files with SPDX version
    Simple seach and replace, keeping the copyright name in the text.

--- corec/corec/str/str_osx.c
+++ corec/corec/str/str_osx.c
@@ -1,29 +1,8 @@
 /*****************************************************************************
- * 
+ *
  * Copyright (c) 2008-2010, CoreCodec, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of CoreCodec, Inc. nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY CoreCodec, Inc. ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL CoreCodec, Inc. BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (C) CoreCodec, Inc.
+ * SPDX-License-Identifier: BSD-3-Clause
  *
  ****************************************************************************/
 
@@ -33,7 +12,7 @@
 
 #include <locale.h>
 
-int tcscmp(const tchar_t* a,const tchar_t* b) 
+int tcscmp(const tchar_t* a,const tchar_t* b)
 {
 #ifdef UNICODE
 	return wcscmp(a,b);
@@ -42,7 +21,7 @@ int tcscmp(const tchar_t* a,const tchar_t* b)
 #endif
 }
 
-int tcsncmp(const tchar_t* a,const tchar_t* b,size_t n) 
+int tcsncmp(const tchar_t* a,const tchar_t* b,size_t n)
 {
 #ifdef UNICODE
 	return wcsncmp(a,b,n);
@@ -51,7 +30,7 @@ int tcsncmp(const tchar_t* a,const tchar_t* b,size_t n)
 #endif
 }
 
-int tcsicmp(const tchar_t* a,const tchar_t* b) 
+int tcsicmp(const tchar_t* a,const tchar_t* b)
 {
 #ifdef UNICODE
     fprintf(stderr, "Not supported yet: %s\n", __FUNCTION__);
@@ -61,7 +40,7 @@ int tcsicmp(const tchar_t* a,const tchar_t* b)
 #endif
 }
 
-int tcsnicmp(const tchar_t* a,const tchar_t* b,size_t n) 
+int tcsnicmp(const tchar_t* a,const tchar_t* b,size_t n)
 {
 #ifdef UNICODE
     fprintf(stderr, "Not supported yet: %s\n", __FUNCTION__);
--- corec/corec/str/str_win32.c
+++ corec/corec/str/str_win32.c
@@ -1,29 +1,8 @@
 /*****************************************************************************
- * 
+ *
  * Copyright (c) 2008-2010, CoreCodec, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of CoreCodec, Inc. nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY CoreCodec, Inc. ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL CoreCodec, Inc. BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (C) CoreCodec, Inc.
+ * SPDX-License-Identifier: BSD-3-Clause
  *
  ****************************************************************************/
 
@@ -42,7 +21,7 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
-int tcsicmp(const tchar_t* a,const tchar_t* b) 
+int tcsicmp(const tchar_t* a,const tchar_t* b)
 {
     int i = CompareString(LOCALE_USER_DEFAULT,NORM_IGNORECASE,a,-1,b,-1);
     if (i)
@@ -56,7 +35,7 @@ int tcsicmp(const tchar_t* a,const tchar_t* b)
 #endif
 }
 
-int tcsnicmp(const tchar_t* a,const tchar_t* b,size_t n) 
+int tcsnicmp(const tchar_t* a,const tchar_t* b,size_t n)
 {
     int i = CompareString(LOCALE_USER_DEFAULT,NORM_IGNORECASE,a,min(tcslen(a),n),b,min(tcslen(b),n));
     if (i)
@@ -70,7 +49,7 @@ int tcsnicmp(const tchar_t* a,const tchar_t* b,size_t n)
 #endif
 }
 
-int tcscmp(const tchar_t* a,const tchar_t* b) 
+int tcscmp(const tchar_t* a,const tchar_t* b)
 {
     int i = CompareString(LOCALE_USER_DEFAULT,0,a,-1,b,-1);
     if (i)
@@ -84,7 +63,7 @@ int tcscmp(const tchar_t* a,const tchar_t* b)
 #endif
 }
 
-int tcsncmp(const tchar_t* a,const tchar_t* b,size_t n) 
+int tcsncmp(const tchar_t* a,const tchar_t* b,size_t n)
 {
     int i = CompareString(LOCALE_USER_DEFAULT,0,a,min(tcslen(a),n),b,min(tcslen(b),n));
     if (i)
