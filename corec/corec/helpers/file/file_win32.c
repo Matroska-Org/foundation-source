89aab820151e9015cd38c53c735622e263207a70
Steve Lhomme <slhomme@matroska.org>
2025-01-01 15:06:19 +0100

    corec: only use _alloca with MSVC
    It doesn't support variable length array in C.

--- corec/corec/helpers/file/file_win32.c
+++ corec/corec/helpers/file/file_win32.c
@@ -287,7 +287,11 @@ static err_t EnumDir(filestream* p,const tchar_t* Exts,bool_t ExtFilter,streamdi
     if (p->DriveNo>=0)
     {
         size_t n = GetLogicalDriveStrings(0,NULL);
+#if defined(_MSC_VER) && !defined (__clang__)
         tchar_t* Drives = _alloca((n+1)*sizeof(tchar_t));
+#else
+        tchar_t Drives[n+1];
+#endif
         if (GetLogicalDriveStrings((DWORD)n,Drives))
         {
             int No = p->DriveNo++;
