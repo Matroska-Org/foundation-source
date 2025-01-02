fa00aefea0d055887f020386fe7b23e382e414a8
Steve Lhomme <slhomme@matroska.org>
2025-01-02 11:40:32 +0100

    corec: remove unused TARGET_IPHONE_DEVICE define

--- corec/corec/portab.h
+++ corec/corec/portab.h
@@ -32,10 +32,6 @@
 #ifdef TARGET_OS_IPHONE // defined in TargetConditionals.h
 #define TARGET_IPHONE
 
-#if !TARGET_IPHONE_SIMULATOR
-#define TARGET_IPHONE_DEVICE
-#endif
-
 
 #if defined(__arm__)
 #define ARM
