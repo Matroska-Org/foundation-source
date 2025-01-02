2b47ace82262c906418a057e24736038e94f91b3
Steve Lhomme <slhomme@matroska.org>
2025-01-02 15:29:14 +0100

    tremor: use labs() for long values

--- libmatroska2/tremor/sharedbook.c
+++ libmatroska2/tremor/sharedbook.c
@@ -212,7 +212,7 @@ ogg_int32_t *_book_unquantize(const static_codebook *b,int n,int *sparsemap,
 	    int index= (j/indexdiv)%quantvals;
 	    int point=0;
 	    int val=VFLOAT_MULTI(delta,delpoint,
-				 abs(b->quantlist[index]),&point);
+				 labs(b->quantlist[index]),&point);
 
 	    val=VFLOAT_ADD(mindel,minpoint,val,point,&point);
 	    val=VFLOAT_ADD(last,lastpoint,val,point,&point);
@@ -246,7 +246,7 @@ ogg_int32_t *_book_unquantize(const static_codebook *b,int n,int *sparsemap,
 	  for(k=0;k<b->dim;k++){
 	    int point=0;
 	    int val=VFLOAT_MULTI(delta,delpoint,
-				 abs(b->quantlist[j*b->dim+k]),&point);
+				 labs(b->quantlist[j*b->dim+k]),&point);
 
 	    val=VFLOAT_ADD(mindel,minpoint,val,point,&point);
 	    val=VFLOAT_ADD(last,lastpoint,val,point,&point);
